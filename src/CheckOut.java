44d2130786d75b2d478009b139aa557b19a1a1ad
Dil <suprads@mail.uc.edu>
2023-04-26 18:01:50 -0400

    Commit 3

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -4,6 +4,20 @@ public class CheckOut
 {
     private static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
+        double price = SafeInput.getRangedDouble(in, "Enter the price of your item", 0.50, 10.00);
+        System.out.println("Your item costs: $" + price);
 
+            boolean itemYN = SafeInput.getYNConfirm(in,"Do you have another item?");
+            if (itemYN) {
+                double price2 = SafeInput.getRangedDouble(in, "Enter the price of your item", 0.50, 10.00);
+                System.out.println("Your item costs: $" + price2);
+                double total = price + price2;
+                System.out.printf("Your total is: $%.2f", total);
+            }
+            else {
+                double total = price;
+                System.out.printf("Your total is: $%.2f", total);
+
+            }
     }
 }
--- src/Reggie.java
+++ src/Reggie.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class Reggie
 {
+    private static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        String SSN = "";
+        String SSN_RegEx = "^\\d{3}-\\d{2}-\\d{4}$";
+        SSN = SafeInput.getRegExString(in, "Enter your SSN", SSN_RegEx);
+        System.out.println("Your SSN is: " + SSN);
 
+        String UCMnum = "";
+        String UCMNum_RegEx = "^(M|m)\\d{5}$";
+        UCMnum = SafeInput.getRegExString(in, "Enter your UC M number", UCMNum_RegEx);
+        System.out.println("Your UC M number is: " + UCMnum);
+
+        String Menu = "";
+        String Menu_RegEx = "^[OoSsVvQq]$"; // ###-##_####
+        Menu = SafeInput.getRegExString(in, "Enter your menu choice", Menu_RegEx);
+        System.out.println("Your menu choice is: " + Menu);
+
+
+    }
 }
